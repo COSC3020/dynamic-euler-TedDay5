bbbae202b7518bdf111659d60b20e2bbfcd5ae40
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-30 08:38:15 -0700

    restructured factorial and e

--- code.js
+++ code.js
@@ -1,9 +1,26 @@
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    if (n <= 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 2;
+    }
+    
+    var factorial = 2;
+    var marker = factorial + 1;
+
+    while(marker <= n) {
+        factorial = factorial * marker;
+        marker++;
+    }
+    return factorial;
 }
 
 function e(n) {
     if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    else sum = 1.0;
+    for(i = 1; i <= n; i++) {
+        sum += 1.0 / factorial(i);
+    }
+    return sum;
 }
